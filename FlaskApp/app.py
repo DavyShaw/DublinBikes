30f64ebf232ba1f43d11801cc1041ec922803863
Daniel McMahon <daniel40392@gmail.com>
2017-04-22 02:39:34 +0100

    Optimised Marker Drawing on Map - Much Faster!
    Optimised Marker Drawing on Map - Much Faster!

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -30,52 +30,44 @@ def get_db():
         engine = g.engine = connect_to_database()
     return engine
 
-##def station(station_id):
-##    '''this function sends SQL query to database based on station_id number passed in.. Returns JSON'''
-##    
-##    sql = """
-##    SELECT available_bikes, available_bike_stands,last_update from stationDynamic where number = {}
-##    """.format(station_id)
-##    
-##    engine = get_db()
-##    rows = engine.execute(sql).fetchall()  # we use fetchall(), but probably there is only one station
-##    res = [dict(row.items()) for row in rows]  # use this formula to turn the rows into a list of dicts
-##    #data = jsonify(data=res)  # jsonify turns the objects into the correct respose
-##    #print(data)
-##    #return data.get_data(as_text=True) #returns as string?
-##    jdata = json.dumps(res)
-##    #engine.dispose() #may not be right..... should close connection
-##    return jdata
 
 @app.route("/")
 def mapview():
     '''code adapted from http://flaskgooglemaps.pythonanywhere.com/
     http://stackoverflow.com/questions/32201678/how-to-get-json-data-from-a-url-using-flask-in-python'''
-    
-    #with open('./static/Dublin.json') as data_file:
-	#data = json.load(data_file)           
+      
     output = []    
-    
+
+    sql = """SELECT * FROM dublinbikes.stationStatic"""
+             
+    engine = get_db()
+    rows = engine.execute(sql).fetchall()  # we use fetchall(), but probably there is only one station
+    res = [dict(row.items()) for row in rows]  # use this formula to turn the rows into a list of dicts
+    #data = jsonify(data=res)  # jsonify turns the objects into the correct respose
+    data = json.dumps(res)
+    parsedData = json.loads(data)
+
+    #backup dublinbikes key (stephens: 88e5dc7b2582c68724462d2d858361ca93582780)
+    #main dublinbikes key(daniels: c64916b14c557faa49fdf72b8902e4d9ff9afe35)
+    r = requests.get("https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=88e5dc7b2582c68724462d2d858361ca93582780")
+    data2 = r.json()
+            
     #note there is no station 50 so need to split the ranges
-    for i in range(1,103):
+    for i in range(1,101):
         if(i!=50):
-            #backup dublinbikes key (stephens: 88e5dc7b2582c68724462d2d858361ca93582780)
-            #main dublinbikes key(daniels: c64916b14c557faa49fdf72b8902e4d9ff9afe35)
-            r = requests.get("https://api.jcdecaux.com/vls/v1/stations/" + str(i) + "?contract=Dublin&apiKey=88e5dc7b2582c68724462d2d858361ca93582780")
-            data = r.json()
             
             #note: using value of bike station numbers as value of each button...
-            infobox = "Name: " + str(data["name"]) + "<br/>" + "Status: " + str(data["status"]) + "<br/>" + "Available Stands: " + str(data["available_bike_stands"]) + "</br>" +  "Available Bikes: " + str(data["available_bikes"]) + "</br>" + "Historic Info: " + '<input type="submit" name="' + str(data["number"]) + '" value="See More" onclick="showDiv(' + "'" + str(data["name"]) + "'" + "," + "'" +  str(data["number"]) + "'" + ')">'
+            infobox = "Name: " + str(data2[i]["name"]) + "<br/>" + "Status: " + str(data2[i]["status"]) + "<br/>" + "Available Stands: " + str(data2[i]["available_bike_stands"]) + "</br>" +  "Available Bikes: " + str(data2[i]["available_bikes"]) + "</br>" + "Historic Info: " + '<input type="submit" name="' + str(data2[i]["number"]) + '" value="See More" onclick="showDiv(' + "'" + str(data2[i]["name"]) + "'" + "," + "'" +  str(data2[i]["number"]) + "'" + ')">'
             
             output.append(
                     {
                             'icon': icons.dots.blue,
-                            'lat': data["position"]["lat"],
-                            'lng': data["position"]["lng"],
+                            'lat': parsedData[i]["position_lat"],
+                            'lng':parsedData[i]["position_lng"],
                             'infobox': infobox
                     }
                 )
-
+            
         trdmap = Map(
             identifier="trdmap",
             lat=53.350140,
@@ -92,7 +84,7 @@ def mapview():
             markers = output
     )
 
-    station(data["number"])
+    station(data2[i]["number"])
 
     return render_template('index.html', trdmap=trdmap)
 
